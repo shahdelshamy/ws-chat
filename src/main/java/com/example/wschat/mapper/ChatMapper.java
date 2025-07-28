c96b909dcfaa82d07836ecb92d93e8e2ec051ccc
shahdelshamy <134215710+shahdelshamy@users.noreply.github.com>
2025-07-29 00:51:49 +0300

    project structure

--- src/main/java/com/example/wschat/mapper/ChatMapper.java
+++ src/main/java/com/example/wschat/mapper/ChatMapper.java
@@ -1,9 +1,12 @@
 package com.example.wschat.mapper;
 
-
-import lombok.Builder;
+import org.mapstruct.Builder;
+import org.mapstruct.CollectionMappingStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
 
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
 @MapperConfig(collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED)
 public abstract class ChatMapper {
+
 }
